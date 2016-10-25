841d60a5d7e1ef92c8f7662e7dfeaf6b8942689c
Greg Lawrance <greg.lawrance@gmail.com>
2016-10-25 07:58:46 -0700

    drop the domains for mapquest tiles

--- scripts/leafletAPI.js
+++ scripts/leafletAPI.js
@@ -3,7 +3,7 @@ var map = L.map('map-leaflet').setView([52.362183, -125.994644], 5);
 
 L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    subdomains: ["otile1", "otile2", "otile3", "otile4"],
+    
     maxZoom: 18
 }).addTo(map);
 
